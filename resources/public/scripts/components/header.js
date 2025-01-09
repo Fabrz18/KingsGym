013e7cab249259f7fa9b0c952cff108c07c9dbce
Fabrz18 <fabriziogarrogarcia@gmail.com>
2025-01-08 19:33:10 -0500

    Responsive Solved

--- resources/public/scripts/components/header.js
+++ resources/public/scripts/components/header.js
@@ -29,3 +29,35 @@ window.addEventListener("resize", function () {
         logo.style.display = "";
     }
 });
+
+window.addEventListener("DOMContentLoaded", function () {
+    const element1 = document.getElementById("Gyms");
+    const element2 = document.getElementById("Reservations");
+    const inscribete = document.getElementById("Inscribite");
+    const logo = document.getElementById("logoIco");
+    const ulLogo = document.getElementById("principal-navigationId");
+
+    if (window.innerWidth <= 650) {
+        element1.style.display = "none";
+        element2.style.display = "none";
+    } else {
+        element1.style.display = "";
+        element2.style.display = "";
+    }
+
+    if (window.innerWidth <= 500) {
+        inscribete.style.fontSize = "15px";
+        ulLogo.style.gap = "0px";
+        console.log("ESTAMOS EN -500px MI GENTE");
+    } else {
+        inscribete.style.fontSize = "";
+        ulLogo.style.gap = "";
+    }
+
+    if (window.innerWidth <= 360) {
+        logo.style.display = "none";
+        
+    } else {
+        logo.style.display = "";
+    }
+});
--- resources/public/scripts/pages/index/promotions.js
+++ resources/public/scripts/pages/index/promotions.js
@@ -14,7 +14,7 @@ window.addEventListener("resize",function(){
 
 window.addEventListener("DOMContentLoaded", function () {
     const DivPromotion = document.getElementById("PostPromotionsId")
-    if (window.innerWidth == 600){
+    if (window.innerWidth <= 600){
         DivPromotion.innerHTML = `
             <a class="" href=""><img src="https://assets.smartfit.com.br/attachments/7901eb1f61659184b94c3b1180dd284d544dd8e1/store/53a728cc5f8158b650c1dd09c2c45a014016bd6b9e43073d144d8177209d/banner_mobile" alt="NewPromotion" title="Nuevas Promociones"></a>
         `;
